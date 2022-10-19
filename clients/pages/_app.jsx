7487bc8ea7b433809efebd082b9028d5322a73e3
BMETEHRI <bamhammed.metehri@ynov.com>
2022-10-19 14:48:24 +0200

    Ajouter le footer | Bam | 14:47 | 19/10/2022

--- clients/pages/_app.jsx
+++ clients/pages/_app.jsx
@@ -1,5 +1,6 @@
 import '../styles/globals.scss'
 import '../styles/home.css'
+import '../styles/footer.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- clients/public/components/Footer.jsx
+++ clients/public/components/Footer.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <p> All rights reserved 2022 &Copy; </p>
-            <ul>
-                <li><a href="#"> Terms & conditions </a></li>
-            </ul>
-            <ul>
-                <li><a href="#"> About us </a></li>
-            </ul>
-            <ul>
-                <li><a href="#"> Contact </a></li>
-            </ul>
-            <ul>
-                <li><a href="#"> Developpers </a></li>
-            </ul>
+        <div className='divfooter'>
+            <div></div>
+            <footer>
+                <p> All rights reserved 2022 &copy; </p>
+                <ul>
+                    <li><a href="#"> Terms & conditions </a></li>
+                </ul>
+                <ul>
+                    <li><a href="#"> About us </a></li>
+                </ul>
+                <ul>
+                    <li><a href="#"> Contact </a></li>
+                </ul>
+                <ul>
+                    <li><a href="#"> Developpers </a></li>
+                </ul>
+            </footer>
+            <div></div>
         </div>
     );
 }
