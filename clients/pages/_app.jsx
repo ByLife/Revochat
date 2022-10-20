91528957b6235d40173265884d4de55a5cbee9a7
thomas78125 <thomas78125@gmail.com>
2022-10-20 11:09:13 +0200

    workspace

--- clients/pages/_app.jsx
+++ clients/pages/_app.jsx
@@ -2,6 +2,9 @@ import '../styles/globals.scss'
 import '../styles/footer.scss'
 import '../styles/navbar.scss'
 import '../styles/home.scss'
+import React from 'react';
+
+export const Context = React.createContext();
 
 
 function MyApp({ Component, pageProps }) {
--- clients/pages/index.jsx
+++ clients/pages/index.jsx
@@ -1,19 +1,30 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../public/components/Footer';
-import HomeComponent from '../public/components/HomeComponent';
 import NavBar from '../public/components/NavBar';
 import HomePage from './HomePage';
 import Home from '../public/components/Home';
+import {Context} from './Context'
+import AppPage from './AppPage'
 
 const Index = () => {
+
+  const [Auth, setAuth] = useState(false);
   return (
-    <div>
-      <NavBar/>
-        <HomePage/>
-        <Home/>
-       <Footer/>
-    </div>
+    <Context.Provider value={[Auth, setAuth]}>
+        
+          {/* <HomePage/> */}
+          {Auth? 
+            <AppPage/>
+
+             :<>
+             <HomePage/>
+                {/* <NavBar/>
+                <Home/>
+                <Footer/> */}
+              </>
+      }
+    </Context.Provider>
   );
 }
 
