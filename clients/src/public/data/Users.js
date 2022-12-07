c6e7f5f190d1952b2d8501f346195126337376fa
thomas78125 <thomas78125@gmail.com>
2022-12-07 10:24:22 +0100

    data

--- clients/src/public/data/Users.js
+++ clients/src/public/data/Users.js
@@ -7,38 +7,38 @@ export const USERS =[
     },
     {
         id: 2,
-        name: 'Antoine',
+        name: 'ByLife',
         image:  'https://www.comassel.com/wp-content/uploads/2021/05/1416404_alert222_script-alert-1-script-1-1.png',
         status: 'online'
     },
     {
         id: 3,
-        name: 'Andri',
+        name: 'Naywvi',
         image:  'https://cdn-icons-png.flaticon.com/512/194/194938.png',
         status: 'online'
     },
     
     {
         id: 4,
-        name: 'Gab',
+        name: 'Lux',
         image:  'https://www.comassel.com/wp-content/uploads/2021/05/1416404_alert222_script-alert-1-script-1-1.png',
         status: 'online'
     },
     {
         id: 5,
-        name: 'Guesnon',
+        name: 'Hocine',
         image:  'https://www.comassel.com/wp-content/uploads/2021/05/1416404_alert222_script-alert-1-script-1-1.png',
         status: 'offline'
     },
     {
         id: 6,
-        name: 'Paul',
+        name: 'Bamhammed',
         image:  'https://www.comassel.com/wp-content/uploads/2021/05/8_avatar-512-1-1.png',
         status: 'blocked'
     },
     {
         id: 7,
-        name: 'Valentin',
+        name: 'taiLee',
         image:  'https://extranetv2.hosgan.com/uploads/temoignages/avatar-513.png',
         status: 'offline'
     },
